2a4a936ccd255f02a8e45320162aed0d8fe53762
Alessandro0003 <alessandroquinta3@gmail.com>
2025-10-22 22:38:00 -0300

    feat: create routes to courses

--- src/modules/courses/routes/index.ts
+++ src/modules/courses/routes/index.ts
@@ -1,13 +1,12 @@
 import type { FastifyPluginAsync } from "fastify";
 import type { ZodTypeProvider } from "fastify-type-provider-zod";
+import * as controllers from "../controllers/index.ts";
+import { createCourse } from "../controllers/schema.ts";
 
 export const coursesRoutes: FastifyPluginAsync = async (app) => {
 	const route = app.withTypeProvider<ZodTypeProvider>();
 
-	route.get("/", () => {
-		return { message: "List all courses" };
-	});
-	route.post("/", () => {
-		return { message: "Create a new course" };
-	});
+	route.get("/", controllers.getCourses);
+
+	route.post("/", { schema: createCourse }, controllers.createCourse);
 };
