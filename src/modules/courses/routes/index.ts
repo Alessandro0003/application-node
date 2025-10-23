320f29207acaccd0b43a66c25c53106289c1b707
Alessandro0003 <alessandroquinta3@gmail.com>
2025-10-22 23:18:37 -0300

    feat: passing schema to validate params route

--- src/modules/courses/routes/index.ts
+++ src/modules/courses/routes/index.ts
@@ -1,12 +1,12 @@
 import type { FastifyPluginAsync } from "fastify";
 import type { ZodTypeProvider } from "fastify-type-provider-zod";
 import * as controllers from "../controllers/index.ts";
-import { createCourse } from "../controllers/schema.ts";
+import { createCourse, getCourseById } from "../controllers/schema.ts";
 
 export const coursesRoutes: FastifyPluginAsync = async (app) => {
 	const route = app.withTypeProvider<ZodTypeProvider>();
 
-	route.get("/:id", controllers.getCourseById);
+	route.get("/:id", { schema: getCourseById }, controllers.getCourseById);
 	route.get("/", controllers.getCourses);
 	route.post("/", { schema: createCourse }, controllers.createCourse);
 };
