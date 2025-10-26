403a2ce468f7683c3fcba25214b2cbf8cf80170c
Alessandro0003 <alessandroquinta3@gmail.com>
2025-10-25 22:54:08 -0300

    feat: impoving documentation

--- src/modules/courses/controllers/schema.ts
+++ src/modules/courses/controllers/schema.ts
@@ -7,11 +7,13 @@ export const getCourseById = z.object({
 });
 
 export const getCourses = z.object({
-	query: z.object({
-		search: z.string().optional(),
-		orderBy: z.enum(["title", "id"]).optional().default("title"),
-		page: z.coerce.number().optional().default(1),
-	}),
+	query: z
+		.object({
+			search: z.string().optional(),
+			orderBy: z.enum(["title", "id"]).optional().default("title"),
+			page: z.coerce.number().optional().default(1),
+		})
+		.optional(),
 });
 
 export const createCourse = z.object({
--- src/modules/courses/routes/index.ts
+++ src/modules/courses/routes/index.ts
@@ -10,7 +10,36 @@ import {
 export const coursesRoutes: FastifyPluginAsync = async (app) => {
 	const route = app.withTypeProvider<ZodTypeProvider>();
 
-	route.get("/:id", { schema: getCourseById }, controllers.getCourseById);
-	route.get("/", { schema: getCourses }, controllers.getCourses);
-	route.post("/", { schema: createCourse }, controllers.createCourse);
+	route.get(
+		"/:id",
+		{
+			schema: {
+				tags: ["Courses"],
+				params: getCourseById,
+			},
+		},
+		controllers.getCourseById,
+	);
+
+	route.get(
+		"/",
+		{
+			schema: {
+				tags: ["Courses"],
+				querystring: getCourses,
+			},
+		},
+		controllers.getCourses,
+	);
+
+	route.post(
+		"/",
+		{
+			schema: {
+				tags: ["Courses"],
+				body: createCourse,
+			},
+		},
+		controllers.createCourse,
+	);
 };
