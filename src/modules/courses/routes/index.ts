0df9b08f4997182dbbb8e58dfe8818d41e47a34e
Alessandro0003 <alessandroquinta3@gmail.com>
2025-10-25 22:38:49 -0300

    feat:  add params to search e pagination to courese

--- src/modules/courses/routes/index.ts
+++ src/modules/courses/routes/index.ts
@@ -1,12 +1,16 @@
 import type { FastifyPluginAsync } from "fastify";
 import type { ZodTypeProvider } from "fastify-type-provider-zod";
 import * as controllers from "../controllers/index.ts";
-import { createCourse, getCourseById } from "../controllers/schema.ts";
+import {
+	createCourse,
+	getCourseById,
+	getCourses,
+} from "../controllers/schema.ts";
 
 export const coursesRoutes: FastifyPluginAsync = async (app) => {
 	const route = app.withTypeProvider<ZodTypeProvider>();
 
 	route.get("/:id", { schema: getCourseById }, controllers.getCourseById);
-	route.get("/", controllers.getCourses);
+	route.get("/", { schema: getCourses }, controllers.getCourses);
 	route.post("/", { schema: createCourse }, controllers.createCourse);
 };
