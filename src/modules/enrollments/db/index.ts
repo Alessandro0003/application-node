7150a8aea7631d40639d6133282dd59f1978259d
Alessandro0003 <alessandroquinta3@gmail.com>
2025-10-27 00:07:26 -0300

    feat: add fields unique to table enrollments

--- src/modules/enrollments/db/index.ts
+++ src/modules/enrollments/db/index.ts
@@ -1,19 +1,23 @@
-import { pgTable, timestamp, uuid } from "drizzle-orm/pg-core";
+import { pgTable, timestamp, uniqueIndex, uuid } from "drizzle-orm/pg-core";
 import { schema } from "../../../database/schema.ts";
 
 const { users, courses } = schema;
 
-export const enrollments = pgTable("enrollments", {
-	id: uuid().primaryKey().defaultRandom(),
-	userId: uuid()
-		.notNull()
-		.references(() => users.id),
-	courseId: uuid()
-		.notNull()
-		.references(() => courses.id),
-	createdAt: timestamp({
-		withTimezone: true,
-	})
-		.notNull()
-		.defaultNow(),
-});
+export const enrollments = pgTable(
+	"enrollments",
+	{
+		id: uuid().primaryKey().defaultRandom(),
+		userId: uuid()
+			.notNull()
+			.references(() => users.id),
+		courseId: uuid()
+			.notNull()
+			.references(() => courses.id),
+		createdAt: timestamp({
+			withTimezone: true,
+		})
+			.notNull()
+			.defaultNow(),
+	},
+	(table) => [uniqueIndex().on(table.userId, table.courseId)],
+);
