50fec85c74d18aea7f6144db4ec8bcff912122a0
Alessandro0003 <alessandroquinta3@gmail.com>
2025-11-18 16:57:10 -0300

    test(E2E): create test to list user

---
+++ test/modules/users/get-users.test.ts
@@ -0,0 +1,35 @@
+import request from "supertest";
+import { beforeEach, describe, expect, it } from "vitest";
+import { app } from "../../../src/app";
+import { db } from "../../../src/database/client";
+import { schema } from "../../../src/database/schema";
+import { makeUsers } from "../../factories/make-users";
+
+const { users } = schema;
+
+describe("Get Users", async () => {
+	await app.ready();
+
+	beforeEach(async () => {
+		await db.delete(users);
+	});
+
+	it("should be able to get a list of users", async () => {
+		const user = await makeUsers({});
+
+		const response = await request(app.server).get("/users");
+
+		expect(response.body).toEqual({
+			statusCode: 200,
+			message: "Users retrieved successfully",
+			data: [
+				{
+					id: user.id,
+					name: user.name,
+					email: user.email,
+					role: user.role,
+				},
+			],
+		});
+	});
+});
