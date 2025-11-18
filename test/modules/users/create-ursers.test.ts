6e4fc9d540400849e998155bbfce310e05b0fe95
Alessandro0003 <alessandroquinta3@gmail.com>
2025-11-18 16:37:14 -0300

    test(E2E): create test to flow creating users

---
+++ test/modules/users/create-ursers.test.ts
@@ -0,0 +1,51 @@
+import { faker } from "@faker-js/faker";
+import request from "supertest";
+import { describe, expect, it } from "vitest";
+import { app } from "../../../src/app";
+
+describe("Create a users", async () => {
+	await app.ready();
+
+	it.skip("should be able to create a new user", async () => {
+		const response = await request(app.server)
+			.post("/users")
+			.set("Content-Type", "application/json")
+			.send({
+				name: faker.person.fullName(),
+				email: faker.internet.email(),
+				password: faker.internet.password(),
+			});
+
+		expect(response.body).toEqual({
+			statusCode: 201,
+			message: "User created successfully",
+		});
+	});
+
+	it("should not be able to create a user with an existing email", async () => {
+		const email = faker.internet.email();
+
+		await request(app.server)
+			.post("/users")
+			.set("Content-Type", "application/json")
+			.send({
+				name: faker.person.fullName(),
+				email,
+				password: faker.internet.password(),
+			});
+
+		const response = await request(app.server)
+			.post("/users")
+			.set("Content-Type", "application/json")
+			.send({
+				name: faker.person.fullName(),
+				email,
+				password: faker.internet.password(),
+			});
+
+		expect(response.body).toEqual({
+			statusCode: 500,
+			message: "Email already exists.",
+		});
+	});
+});
