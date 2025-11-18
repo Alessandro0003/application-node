7b699c1031e4f43760aa1ae2ef1d2997fb00274f
Alessandro0003 <alessandroquinta3@gmail.com>
2025-11-18 20:14:26 -0300

    refactor: change export entities

--- src/database/schema.ts
+++ src/database/schema.ts
@@ -1,7 +1,4 @@
 import { courses } from "../modules/courses/db/index.ts";
 import { users } from "../modules/users/db/index.ts";
 
-export const schema = {
-	courses,
-	users,
-};
+export { courses, users };
