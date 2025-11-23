821d721c7b7fbb96e8b882bfe1015a8615e3a1e6
Alessandro0003 <alessandroquinta3@gmail.com>
2025-11-22 23:54:45 -0300

    refactor: change type to params request application

--- src/types/request.ts
+++ src/types/request.ts
@@ -1,7 +1,9 @@
 import type { FastifyRequest } from "fastify";
 import type { z } from "zod";
 
-export type SchemaToRequest<T extends z.ZodSchema> = FastifyRequest<{
+export type SchemaToRequest<
+	T extends z.ZodSchema<{ params?: unknown; body?: unknown; query?: unknown }>,
+> = FastifyRequest<{
 	Params: z.infer<T>["params"];
 	Body: z.infer<T>["body"];
 	Querystring: z.infer<T>["query"];
