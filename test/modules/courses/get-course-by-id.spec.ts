c972b2119d17a6aef6111fa6aeb63e5c21b36e7b
Alessandro0003 <alessandroquinta3@gmail.com>
2025-11-24 22:59:27 -0300

    test(e2e): change test to execute route getCourseById with authenticate

--- test/modules/courses/get-course-by-id.spec.ts
+++ test/modules/courses/get-course-by-id.spec.ts
@@ -2,13 +2,17 @@ import request from "supertest";
 import { expect, test } from "vitest";
 import { app } from "../../../src/app.ts";
 import { makeCourse } from "../../factories/make-course.ts";
+import { makeAuthenticateUser } from "../../factories/make-users.ts";
 
 test("get course by id", async () => {
 	await app.ready();
 
+	const { token } = await makeAuthenticateUser("student");
 	const course = await makeCourse();
 
-	const response = await request(app.server).get(`/courses/${course.id}`);
+	const response = await request(app.server)
+		.get(`/courses/${course.id}`)
+		.set("Authorization", token);
 
 	expect(response.body).toEqual({
 		statusCode: 200,
