d96fce198ea7c12392c8751bcb48221bc2ea8aec
Alessandro0003 <alessandroquinta3@gmail.com>
2025-11-24 22:58:21 -0300

    feat: creating makeAuthenticateUser to authenticate

--- test/factories/make-users.ts
+++ test/factories/make-users.ts
@@ -1,10 +1,12 @@
 import { randomUUID } from "node:crypto";
 import { faker } from "@faker-js/faker";
 import { hash } from "argon2";
+import jwt from "jsonwebtoken";
+import { config } from "../../src/config/index.ts";
 import { db } from "../../src/database/client.ts";
 import { users } from "../../src/database/schema.ts";
 
-export const makeUsers = async () => {
+export const makeUsers = async (role?: "manager" | "student") => {
 	const passwordBeforeHash = randomUUID();
 
 	const result = await db
@@ -13,6 +15,7 @@ export const makeUsers = async () => {
 			email: faker.internet.email(),
 			name: faker.person.fullName(),
 			password: await hash(passwordBeforeHash),
+			role,
 		})
 		.returning();
 
@@ -20,4 +23,15 @@ export const makeUsers = async () => {
 		user: result[0],
 		passwordBeforeHash,
 	};
+};
+
+export const makeAuthenticateUser = async (role: "manager" | "student") => {
+	const { user } = await makeUsers();
+
+	const token = jwt.sign(
+		{ sub: user.id, role: user.role },
+		config.jwt.JWT_SECRET,
+	);
+
+	return { user, token };
 };
