4a48afef401619f6849f6363663162fa730513d6
Alessandro0003 <alessandroquinta3@gmail.com>
2025-11-24 22:59:49 -0300

    feat: create middleware to checkUserRole

--- src/middleware/check-user-role.ts
+++ src/middleware/check-user-role.ts
@@ -1,13 +1,12 @@
 import type { FastifyReply, FastifyRequest } from "fastify";
 import { getAuthenticatedUserRequest } from "../utils/get-authenticated-user-request.ts";
 
-export const checkUserRole = async (
-	req: FastifyRequest,
-	reply: FastifyReply,
-) => {
-	const user = getAuthenticatedUserRequest(req);
+export const checkUserRole = (role: string) => {
+	return async (req: FastifyRequest, reply: FastifyReply) => {
+		const user = getAuthenticatedUserRequest(req);
 
-	if (user.role !== "manager") {
-		return reply.status(401).send({ message: "Unauthorized" });
-	}
+		if (user.role !== role) {
+			return reply.status(401).send({ message: "Unauthorized" });
+		}
+	};
 };
