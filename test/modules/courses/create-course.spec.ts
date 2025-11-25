f3ef286ab8286ddbeccd724c3dbae8a9ee1bf038
Alessandro0003 <alessandroquinta3@gmail.com>
2025-11-24 23:09:58 -0300

    test(e2e): change test to passing authenticate to route

--- test/modules/courses/create-course.spec.ts
+++ test/modules/courses/create-course.spec.ts
@@ -2,14 +2,18 @@ import { faker } from "@faker-js/faker";
 import request from "supertest";
 import { expect, test } from "vitest";
 import { app } from "../../../src/app.ts";
+import { makeAuthenticateUser } from "../../factories/make-users.ts";
 
 test("create a course", async () => {
 	await app.ready();
 
+	const { token } = await makeAuthenticateUser("manager");
+
 	const response = await request(app.server)
 		.post("/courses")
 		.set("Content-Type", "application/json")
-		.send({ title: faker.lorem.words(3) });
+		.set("Authorization", token)
+		.send({ title: faker.lorem.words(4) });
 
 	expect(response.body).toEqual({
 		statusCode: 201,
