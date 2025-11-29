b8d1060941aad939faff1d0e88c854a5f19d8534
Alessandro0003 <alessandroquinta3@gmail.com>
2025-11-29 20:29:52 -0300

    refactor: add host to application

--- src/server.ts
+++ src/server.ts
@@ -1,6 +1,6 @@
 import "dotenv/config";
 import { app } from "./app.ts";
 
-app.listen({ port: 3333 }).then(() => {
+app.listen({ port: 3333, host: "0.0.0.0" }).then(() => {
 	console.log("HTTP server running!");
 });
